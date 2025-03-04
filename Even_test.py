e53582fd6a30a54241a714eacdced58dadfcffb9
schoolacco <127917983+schoolacco@users.noreply.github.com>
2025-03-05 10:09:43 +1100

    Update Even_test.py

--- Even_test.py
+++ Even_test.py
@@ -1,5 +1,6 @@
 import requests
-
+import os
+import time
 # API Base URL
 API_URL = "https://api.isevenapi.xyz/api/iseven/"
 def is_even(num):
@@ -11,14 +12,17 @@ def is_even(num):
     else:
        raise KeyError
     return result
-try:
-  even = is_even(input("Enter a number (between 0-999,999): "))
-  if even["iseven"]:
-     print("The number is even.")
-  else:
-     print("Your number is odd.")
-  print(even["ad"]+"\nDeveloper's note: Sorry we can't remove the ads :(")
-except KeyError:
-   print("Enter a NUMBER!")
-except SyntaxError:
-   print("Enter a number within the range.")
+while True:
+  try:
+    even = is_even(input("Enter a number (between 0-999,999): "))
+    if even["iseven"]:
+       print("The number is even.")
+    else:
+       print("Your number is odd.")
+    print(even["ad"]+"\nDeveloper's note: Sorry we can't remove the ads :(")
+  except KeyError:
+     print("Enter a NUMBER!")
+  except SyntaxError:
+     print("Enter a number within the range.")
+  time.sleep(5)
+  os.system('cls')
