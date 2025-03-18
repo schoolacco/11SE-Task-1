8b2185bc6f07b6ac6136603d102eda7e1dff6853
schoolacco <127917983+schoolacco@users.noreply.github.com>
2025-03-19 10:09:44 +1100

    Improved both the API and Image modules, changed documentation goals due to API rate limits
    Added several necessary details required to improve the functionality of the program and the aesthetics

--- Documentation.md
+++ Documentation.md
@@ -11,9 +11,8 @@
 ---
 
 1. The user must be capable of obtaining the Astronomy Picture Of the Day and any related data including the title, date and explanation of what the picture is. They must be capable of retriving this data for any previous APOD dates.
-2. It should also be capable of accessing NASA's 'Insight' being capable of retriving data about Mars' current weather.
-3. It must also be capable of retriving images from the Mars rovers (and data about those rovers) and display those images.
-4. It should be able of accessing NASA's photos of Earth.
+2. It should be able of accessing NASA's photos of Earth.
+(More would be done... but rate limits exist)
 
 ---
 
--- api_module.py
+++ api_module.py
@@ -4,14 +4,15 @@ import webview
 # NASA API Base URL
 APOD_URL = "https://api.nasa.gov/planetary/apod"
 EARTH_URL = "https://api.nasa.gov/planetary/earth/assets"
+ROVER_URL = "https://api.nasa.gov/neo/rest/v1"
 API_KEY = "WEIouyu7zWA7RuTEsuAJPVYTcaKeNyhIGr6Fn6bV"
 # Dictionary to store favorite celestial objects
 favorites = {}
 class apod:
   #running = False
-  def get_apod():
+  def get_apod(date):
       """Fetch NASA's Astronomy Picture of the Day (APOD)."""
-      params = {"api_key": API_KEY}
+      params = {"api_key": API_KEY, "date": date}
       response = requests.get(APOD_URL, params=params)
       if response.status_code == 200:
           data = response.json()
@@ -43,16 +44,26 @@ class apod:
             data = response.json()
             return webbrowser.open(data["url"])
     except NameError:
+       print("Unable to retrive Image URL")
        pass
+  def return_explanation(date):
+     try:
+        params = {"api_key": API_KEY, "date": date}
+        response = requests.get(APOD_URL, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            return data["explanation"]
+     except NameError:
+       return "Unable to retrieve explanation"
 class Earth:
      def open_image(lat, lon, dim, date):
-      """Fetch an Earth pciture."""
+      """Fetch an Earth picture."""
       try:
         params = {"api_key": API_KEY, "lat": lat, "lon": lon, "dim": dim, "date": date}
         response = requests.get(EARTH_URL, params=params)
         if response.status_code == 200:
             data = response.json()
-            window = webview.create_window(date["date"], data["url"]) #Opens the image url in a GUI with the title of the image
+            window = webview.create_window(data["date"], data["url"]) #Opens the image url in a GUI with the title of the image
             webview.start() #Open the GUI
             return window
         else:
@@ -60,3 +71,13 @@ class Earth:
             return None
       except NameError:
        pass
+     def open_url(lat, lon, dim, date):
+       try:
+           params = {"api_key": API_KEY, "lat": lat, "lon": lon, "dim": dim, "date": date}
+           response = requests.get(EARTH_URL, params=params)
+           if response.status_code == 200:
+               data = response.json()
+               return webbrowser.open(data["url"])
+       except NameError:
+          print("Unable to access image URL")
+          pass   
